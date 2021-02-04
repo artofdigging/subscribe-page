32d8467ed286faeb60b9e532d3ac8e19f09876d7
Christopher Norman <chris@artofdigging.com>
2021-02-04 10:44:10 -0800

    Adding google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,14 @@ export default class MyDocument extends Document {
         <Head>
           <link href="//fonts.googleapis.com/css?family=Poppins:400,600,700" rel="stylesheet" title="Google Fonts" type="text/css"></link>
           <script data-ad-client="ca-pub-9580600822475194" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-11G7QJGR3Z"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-11G7QJGR3Z');
+          </script>
         </Head>
         <body>
           <Main />
